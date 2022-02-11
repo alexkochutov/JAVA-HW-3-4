3d101e4b6e48a373651e29f1270e0ec988e82d7d
alexkochutov <alex.kochutov@gmail.com>
2022-02-11 13:59:38 +0300

    Defined class FilmManager + @NoArgsConstructor + @AllArgsConstructor + @Getter + @Setter

--- src/main/java/ru/netology/domain/FilmManager.java
+++ src/main/java/ru/netology/domain/FilmManager.java
@@ -1,4 +1,21 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class FilmManager {
+    private Film[] poster = new Film[0];
+    private int posterLimit = 10;
+
+    public FilmManager(int posterLimit) {
+        this.posterLimit = posterLimit;
+    }
+
+
 }
