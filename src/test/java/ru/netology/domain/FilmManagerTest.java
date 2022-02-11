22f79011a06886dcf2c0a4ba3f33f068cdb66b94
alexkochutov <alex.kochutov@gmail.com>
2022-02-11 14:16:16 +0300

    Added file FilmManagerTest.java

---
+++ src/test/java/ru/netology/domain/FilmManagerTest.java
@@ -0,0 +1,27 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmManagerTest {
+
+    @Test
+    void shouldAddFilms() {
+        FilmManager manager = new FilmManager();
+        Film first = new Film(1, "film", "film");
+        Film second = new Film(2, "film", "film");
+        Film third = new Film(3, "film", "film");
+        Film fourth = new Film(4, "film", "film");
+        Film fifth = new Film(5, "film", "film");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Film[] expected = {first, second, third, fourth, fifth};
+        assertArrayEquals(expected, manager.getPoster());
+    }
+}
