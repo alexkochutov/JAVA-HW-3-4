a514fe7c7beb81791b608f7989f868265628385b
alexkochutov <alex.kochutov@gmail.com>
2022-02-13 22:09:49 +0300

    Added FilmManager.java (business logic)

--- src/main/java/ru/netology/domain/FilmManager.java
+++ src/main/java/ru/netology/domain/FilmManager.java
@@ -8,5 +8,29 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FilmManager {
+    private Repository repo = new Repository();
 
+    public FilmManager(Repository repo) {
+        this.setRepo(repo);
+    }
+
+    public void save(Film item) {
+        repo.save(item);
+    }
+
+    public Film[] findAll() {
+        return repo.findAll();
+    }
+
+    public Film findByID(int ID) {
+        return repo.findByID(ID);
+    }
+
+    public void removeByID(int ID) {
+        repo.removeByID(ID);
+    }
+
+    public void removeAll() {
+        repo.removeAll();
+    }
 }
